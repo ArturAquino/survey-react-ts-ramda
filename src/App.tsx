74452604640991c9175927f2c61dcaf03b6742ba
Eduardo Gonçalves Costa <eduardogc@outlook.com>
2019-04-21 10:38:37 -0300

    fixing incorrect rule for gh-pages

--- src/App.tsx
+++ src/App.tsx
@@ -10,7 +10,7 @@ import NotFound from "./pages/NotFound/NotFound";
 class App extends React.Component {
   public render() {
     return (
-      <BrowserRouter>
+      <BrowserRouter basename="/survey-react-ts-ramda">
         <Switch>
           <Route path="/" exact component={NPS} />
           <Route path="/justify" component={Justify} />
